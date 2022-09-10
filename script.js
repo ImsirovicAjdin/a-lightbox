4e0831135a11ca00655ecfb5dfee3a6a6e6da8e8
Ajdin Imsirovic <ajdika@live.com>
2022-09-10 14:20:00 +0200

    Complete a working modal on img click

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const img = document.getElementById('triggerModal');
 const modal = document.getElementById('modal-overlay');
+const closeClass = document.getElementsByClassName('close');
 
 img.addEventListener('click', () => {
     modal.classList.toggle('show')
 })
+
+for (let i = 0; i < closeClass.length; i++) {
+    closeClass[i].addEventListener('click', function() {
+        modal.classList.remove('show');
+    })
+}
