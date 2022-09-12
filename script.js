b262393a4cd832284053179c6a4957a8ae81641d
Ajdin Imsirovic <ajdika@live.com>
2022-09-12 09:55:51 +0200

    Make a semi-working hybrid of slider and modal components

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const img = document.getElementById('triggerModal');
+let counter = 0;
+let imgs = [
+    './imgs/desert-mountain-large-image.jpeg',
+    './imgs/mountain-purple-large-image.jpeg',
+    './imgs/mountain-three-large-image.jpeg',
+];
+
+const modalTriggerImg = [...document.getElementsByClassName('triggerModal')];
 const modal = document.getElementById('modal-overlay');
-const closeClass = document.getElementsByClassName('close');
 
-img.addEventListener('click', () => {
-    modal.classList.toggle('show')
-})
-
-for (let i = 0; i < closeClass.length; i++) {
-    closeClass[i].addEventListener('click', function() {
-        modal.classList.remove('show');
+for(let i = 0; i < modalTriggerImg.length; i++) {
+    modalTriggerImg[i].addEventListener('click', () => {
+        modal.classList.toggle('show');
     })
+}
+
+function nextSlide() {
+    document.slide.src = imgs[counter];
+    console.log(counter);
+    counter < (imgs.length - 1) ? counter++ : counter = 0;
+}
+
+function prevSlide() {
+    if (counter <= 0) {
+        counter = imgs.length - 1;
+    } else {
+        counter--;
+    }
+    img.src = imgs[counter];
 }
